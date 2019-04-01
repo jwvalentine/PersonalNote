8cb2f69cb4da117a738f77740cd1321ad2c4a560
Joe <jwvalentine@outlook.com>
2019-04-01 19:22:46 -0400

    Initial
    First Commit!

---
+++ PersonalNote/Models/WorkoutDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalNote.Models
+{
+    public class WorkoutDBContext : DbContext
+    {
+        public virtual DbSet<WorkoutReport> WorkoutReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=myTestDB;Persist Security Info=True;Integrated Security = true");
+            }
+        }
+    }
+}
---
+++ PersonalNote/Models/WorkoutReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalNote.Models
+{
+    public class WorkoutReport
+    {
+        [Key]
+        public int Id { get; set;}
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime WorkoutDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Category { get; set; }
+
+    }
+}
